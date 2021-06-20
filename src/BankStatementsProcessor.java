451a2b1a2c72b518200a200689ab9844e8d5c400
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 19:00:46 +0200

    Moved functions

---
+++ src/BankStatementsProcessor.java
@@ -0,0 +1,38 @@
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankStatementsProcessor {
+
+    final private List<BankTransaction> bankTransactions;
+
+    BankStatementsProcessor() {
+        final BankTransactionCSVParser bankStatementParser = new BankTransactionCSVParser();
+        final BankStatementsResources resources = new BankStatementsResources();
+        final List<String> resourcesLinesList = resources.getResourcesLinesList();
+
+        this.bankTransactions = bankStatementParser.parseLinesFromCSV(resourcesLinesList);
+    }
+
+    private static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        double total = 0d;
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+
+        return total;
+    }
+
+    private static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+
+        return bankTransactionsInMonth;
+    }
+}
