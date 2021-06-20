a44f4a48b4303ce503997870289500a005d75594
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 16:38:28 +0200

    Refactored class name

--- src/BankStatementsResources.java
+++ src/BankStatementsResources.java
@@ -4,13 +4,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class bankStatements {
+public class BankStatementsResources {
     private static final String RESOURCES = "src/resources.csv";
 
-    public bankStatements() {
+    public BankStatementsResources() {
     }
 
     public static String getRESOURCES() {
+
         return RESOURCES;
     }
 }
