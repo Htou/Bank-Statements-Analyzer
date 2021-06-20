012dd04d41fa3bbe1b31673b6abaf1e12503fb52
Hichem Touati <hichemtouati@outlook.com>
2021-06-21 00:37:54 +0200

    First test

---
+++ src/test/BankStatementCSVParserTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import main.BankStatementCSVParser;
+import main.BankStatementParser;
+import org.junit.jupiter.api.Assertions;
+
+i
+
+
+public class BankStatementCSVParserTest {
+
+    private final BankStatementParser BankstatementParser = new BankStatementCSVParser();
+
+    @Testable
+    public void shouldParseOneCorrectLine() throws Exception {
+        Assertions.fail("Not yet implemented");
+    }
+
+
+}
