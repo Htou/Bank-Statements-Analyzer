006c15b1f3448c4b47f1a6a0b9fae43d6ddc84a9
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 21:52:16 +0200

    refactored code

--- src/BankStatementProcessor.java
+++ src/BankStatementProcessor.java
@@ -10,28 +10,31 @@ public class BankStatementProcessor {
 
     public double calculateTotalAmount() {
         double total = 0;
-        for(final BankTransaction bankTransaction:
-                bankTransactions) {
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
             total += bankTransaction.getAmount();
         }
         return total;
     }
+
     public double calculateTotalInMonth(final Month month) {
-        double total = 0;for(final BankTransaction bankTransaction:
-                bankTransactions) {
-            if(bankTransaction.getDate().getMonth() == month)
-            {
+        double total = 0;
+        for (final BankTransaction bankTransaction : bankTransactions) {
+
+            if (bankTransaction.getDate().getMonth() == month) {
                 total += bankTransaction.getAmount();
             }
         }
+
         return total;
     }
+
     public double calculateTotalForCategory(final String
                                                     category) {
         double total = 0;
-        for(final BankTransaction bankTransaction:
+        for (final BankTransaction bankTransaction :
                 bankTransactions) {
-            if(bankTransaction.getDescription().equals(category)) {
+            if (bankTransaction.getDescription().equals(category)) {
                 total += bankTransaction.getAmount();
             }
         }
