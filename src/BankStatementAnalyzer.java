20d6ff2521a7b4a5f33776f7d8a726ff2dbe6b0f
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 23:34:31 +0200

    Decoupled class

--- src/BankStatementAnalyzer.java
+++ src/BankStatementAnalyzer.java
@@ -2,22 +2,24 @@ import java.time.Month;
 import java.util.List;
 
 public class BankStatementAnalyzer {
-    private final BankStatementProcessor bankStatementProcessor;
 
     BankStatementAnalyzer() {
-        BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
-        BankStatementResources resources = new BankStatementResources();
-        List<String> resourcesLinesList = resources.getResourcesLinesList();
-        List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(resourcesLinesList);
-
-        this.bankStatementProcessor = new BankStatementProcessor(bankTransactions);
     }
 
-    public void collectSummary() {
+    public void collectSummary(BankStatementProcessor bankStatementProcessor) {
         System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
         System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
         System.out.println("The total for transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
         System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+
+    public void analyze(final BankStatementParser bankStatementParser) {
+        BankStatementResources resources = new BankStatementResources();
+        final List<String> lines = resources.getResourcesLinesList();
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+        collectSummary(bankStatementProcessor);
     }
 
 }
