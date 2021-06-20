31d661bbf393421b3aa9bfeb18d74434ff618167
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 21:50:02 +0200

    refactored code

--- src/BankStatementsAnalyzer.java
+++ src/BankStatementsAnalyzer.java
@@ -1,21 +1,24 @@
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementsAnalyzer {
-
-    private final List<BankTransaction> bankTransactions;
+    private BankStatementProcessor bankStatementProcessor;
+    private List<BankTransaction> bankTransactions;
 
     BankStatementsAnalyzer() {
+        BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+        BankStatementsResources resources = new BankStatementsResources();
+        List<String> resourcesLinesList = resources.getResourcesLinesList();
 
+        this.bankTransactions = bankStatementParser.parseLinesFromCSV(resourcesLinesList);
+        this.bankStatementProcessor = new BankStatementProcessor(bankTransactions);
     }
 
-
-
-
-    public void printAmount( ) {
-        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
-        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    public void collectSummary() {
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
+        System.out.println("The total for transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
     }
 
 }
