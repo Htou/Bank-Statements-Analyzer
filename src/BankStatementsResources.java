550d3f1710cf395fb0e6afe506f7029013f861a8
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 17:30:04 +0200

    Moved function

--- src/BankStatementsResources.java
+++ src/BankStatementsResources.java
@@ -2,17 +2,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 public class BankStatementsResources {
+
     private static final String RESOURCES = "src/resources.csv";
 
     public BankStatementsResources() {
     }
 
     public static String getRESOURCES() {
-
         return RESOURCES;
+    }
+
+    public List<String> getResourcesLinesList() {
+        try {
+            String RESOURCES = getRESOURCES();
+            final Path path = Paths.get(RESOURCES);
+            final List<String> lines = Files.readAllLines(path);
+
+            return lines;
+
+        } catch (IOException e) {
+            System.out.println("Error, file not found");
+
+            return Collections.<String>emptyList();
+        }
     }
 
 }
