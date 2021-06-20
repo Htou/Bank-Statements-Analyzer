020c055b31f840b6aa644a19c164865b6061db30
Hichem Touati <hichemtouati@outlook.com>
2021-06-20 15:53:43 +0200

    Decompised functions and added calculate sum january function

--- src/BankStatementsAnalyzer.java
+++ src/BankStatementsAnalyzer.java
@@ -2,34 +2,64 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 
 public class BankStatementsAnalyzer {
 
-
-
-    public void calculateTotal() {
+    public List<String> getResourcesLines() {
         try {
             String RESOURCES = BankTransactions.getRESOURCES();
             final Path path = Paths.get(RESOURCES);
             final List<String> lines = Files.readAllLines(path);
 
-            double total = 0d;
-
-            for (final String line : lines) {
-                final String[] columns = line.split(",");
-                final double amount = Double.parseDouble(columns[1]);
-
-                total += amount;
-
-            }
-
-            System.out.println("The total for all transactions is " + total);
+            return lines;
 
         } catch (Exception e) {
             System.out.println("Error, file not found");
+
+            List<String> emptyList = Collections.<String>emptyList();
+            return emptyList;
+        }
+    }
+
+
+    public void calculateTotal() {
+        List<String> lines = getResourcesLines();
+
+        double total = 0d;
+
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+
+            total += amount;
         }
 
+        System.out.println("The total for all transactions is " + total);
+    }
+
+    public void calculateSumJanuary() {
+        List<String> lines = getResourcesLines();
+
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+
+        System.out.println("The total for all transactions in January\n" +
+                "is" + total);
     }
 
 }
