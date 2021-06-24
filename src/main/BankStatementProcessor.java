8d68f1d9a0a6f995fb36267a4ea762d384012c52
Hichem Touati <hichemtouati@outlook.com>
2021-06-24 12:30:13 +0200

    added a space for return for readability

--- src/main/BankStatementProcessor.java
+++ src/main/BankStatementProcessor.java
@@ -9,16 +9,6 @@ public class BankStatementProcessor {
     public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
         this.bankTransactions = bankTransactions;
     }
-
-    public double calculateTotalAmount() {
-        double total = 0;
-
-        for (final BankTransaction bankTransaction : bankTransactions) {
-            total += bankTransaction.getAmount();
-        }
-        return total;
-    }
-
     public double calculateTotalInMonth(final Month month) {
         double total = 0;
 
@@ -42,6 +32,15 @@ public class BankStatementProcessor {
             }
         }
 
+        return total;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0;
+
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
         return total;
     }
 }
