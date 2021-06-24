e7edc91b15ab26b9ba00c91cbe432563ae45afec
Hichem Touati <hichemtouati@outlook.com>
2021-06-24 12:13:56 +0200

    created mmy first test

--- src/test/BankStatementCSVParserTest.java
+++ src/test/BankStatementCSVParserTest.java
@@ -1,19 +1,32 @@
 package test;
 
+import main.BankTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import main.BankStatementCSVParser;
 import main.BankStatementParser;
-import org.junit.jupiter.api.Assertions;
 
-i
-
+import java.time.LocalDate;
+import java.time.Month;
 
 public class BankStatementCSVParserTest {
 
-    private final BankStatementParser BankstatementParser = new BankStatementCSVParser();
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
 
-    @Testable
+    @Test
     public void shouldParseOneCorrectLine() throws Exception {
-        Assertions.fail("Not yet implemented");
+        final String line = "30-01-2017,-50,Tesco";
+
+        final BankTransaction result = statementParser.parseFrom(line);
+
+        final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        final double tolerance = 0.0d;
+
+        Assertions.assertEquals(expected.getDate(), result.getDate());
+        Assertions.assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+        Assertions.assertEquals(expected.getDescription(), result.getDescription());
+
     }
 
 
