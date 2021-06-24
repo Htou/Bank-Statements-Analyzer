171e89861d2abb1a7d93244fe81b38392e729200
Hichem Touati <hichemtouati@outlook.com>
2021-06-24 12:30:30 +0200

    Added extra prints

--- src/main/BankStatementAnalyzer.java
+++ src/main/BankStatementAnalyzer.java
@@ -9,10 +9,23 @@ public class BankStatementAnalyzer {
     }
 
     public void collectSummary(BankStatementProcessor bankStatementProcessor) {
-        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+
         System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
         System.out.println("The total for transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
-        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+        System.out.println("The total for transactions in March is " + bankStatementProcessor.calculateTotalInMonth(Month.MARCH));
+        System.out.println("The total for transactions in April is " + bankStatementProcessor.calculateTotalInMonth(Month.APRIL));
+        System.out.println("The total for transactions in May is " + bankStatementProcessor.calculateTotalInMonth(Month.MAY));
+        System.out.println("The total for transactions in June is " + bankStatementProcessor.calculateTotalInMonth(Month.JUNE));
+        System.out.println("The total for transactions in July is " + bankStatementProcessor.calculateTotalInMonth(Month.JULY));
+        System.out.println("The total for transactions in August is " + bankStatementProcessor.calculateTotalInMonth(Month.AUGUST));
+        System.out.println("The total for transactions in September is " + bankStatementProcessor.calculateTotalInMonth(Month.SEPTEMBER));
+        System.out.println("The total for transactions in October is " + bankStatementProcessor.calculateTotalInMonth(Month.OCTOBER));
+        System.out.println("The total for transactions in November is " + bankStatementProcessor.calculateTotalInMonth(Month.NOVEMBER));
+        System.out.println("The total for transactions in December is " + bankStatementProcessor.calculateTotalInMonth(Month.DECEMBER));
+
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+
     }
 
     public void analyze(final BankStatementParser bankStatementParser) {
