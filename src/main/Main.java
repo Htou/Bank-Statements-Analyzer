d5ecc1a5c80e8a6d6ef84d462f22bf48f032f5be
htou <h.touati@tutanota.com>
2021-06-25 22:06:44 +0200

    added parameter for exporting html

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(final String... args) {
         final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
         final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+        final Exporter exporter = new HtmlExporter();
 
-        bankStatementAnalyzer.analyze(bankStatementParser);
+        bankStatementAnalyzer.analyze(bankStatementParser, exporter);
     }
 }
