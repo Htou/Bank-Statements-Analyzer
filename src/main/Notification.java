bb91c0b180e5f791c8c34d424732dd18122e7570
htou <h.touati@tutanota.com>
2021-06-25 19:23:35 +0200

    Created errorhandling domain class with notification  pattern

---
+++ src/main/Notification.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Notification {
+    private final ArrayList<String> errors = new ArrayList<String>();
+
+    public void addError(final String message) {
+        this.errors.add(message);
+    }
+
+    public boolean hasErrors() {
+        return !this.errors.isEmpty();
+    }
+
+    public String errorMessage() {
+        return this.errors.toString();
+    }
+
+    public ArrayList<String> getErrors() {
+        return this.errors;
+    }
+}
