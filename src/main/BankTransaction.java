419aced9ab67be0a696eef7edabcd3aa8afa7dbe
htou <h.touati@tutanota.com>
2021-06-25 20:34:56 +0200

    Added BankTransaction validator using the notification pattern

--- src/main/BankTransaction.java
+++ src/main/BankTransaction.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 public class BankTransaction {
@@ -59,5 +60,32 @@ public class BankTransaction {
     public int hashCode() {
 
         return Objects.hash(date, amount, description);
+    }
+
+    public Notification validate() {
+        final Notification notification = new Notification();
+
+        if (this.description.length() > 100) {
+            notification.addError("The description is too long");
+        }
+
+        final LocalDate parsedDate;
+        try {
+            parsedDate = this.date;
+            if (parsedDate.isAfter(LocalDate.now())) {
+                notification.addError("Date cannot be in the future");
+            }
+        } catch (DateTimeParseException e) {
+            notification.addError("Invalid format for date");
+        }
+
+        final double amount;
+        try {
+            amount = this.amount;
+        } catch (NumberFormatException e) {
+            notification.addError("Invalid format for amount");
+        }
+
+        return notification;
     }
 }
